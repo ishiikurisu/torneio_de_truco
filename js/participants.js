66f061238e9f31ef6910a33088a2d94d4b567d4a
Cristiano Silva Jr <crisjr@pm.me>
2020-07-08 09:38:58 -0300

    Clean participant page

--- js/participants.js
+++ js/participants.js
@@ -1,21 +1,21 @@
 const PARTICIPANTS_HTML = `
 <form>
-    <input type="text" name="" class="participantname" value="Jumbo"><br>
-    <input type="text" name="" class="participantname" value="Bayul"><br>
-    <input type="text" name="" class="participantname" value="Rafa"><br>
-    <input type="text" name="" class="participantname" value="Mira"><br>
-    <input type="text" name="" class="participantname" value="Sprocket"><br>
-    <input type="text" name="" class="participantname" value="Big Top"><br>
-    <input type="text" name="" class="participantname" value="Savannah"><br>
-    <input type="text" name="" class="participantname" value="Flora"><br>
-    <input type="text" name="" class="participantname" value="Rolf"><br>
-    <input type="text" name="" class="participantname" value="Tiffany"><br>
-    <input type="text" name="" class="participantname" value="Sally"><br>
-    <input type="text" name="" class="participantname" value="Tom Nook"><br>
-    <input type="text" name="" class="participantname" value="Isabelle"><br>
-    <input type="text" name="" class="participantname" value="Blathers"><br>
-    <input type="text" name="" class="participantname" value="Roger"><br>
-    <input type="text" name="" class="participantname" value="Redd"><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
+    <input type="text" name="" class="participantname" value=""><br>
     <button type="button" name="button" id="tournament_generator">Gerar torneio</button>
 </form>
 `;
